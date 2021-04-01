f91b5e54ea5ed21cbba60df34b6f55a09d8a5a0b
DariaGusarova8192 <dashagysarova@gmail.com>
2021-04-01 17:58:39 +0300

    Fixed 2.3

--- modules/complex-number/test/test_gusarova_daria_complex_number.cpp
+++ modules/complex-number/test/test_gusarova_daria_complex_number.cpp
@@ -5,7 +5,8 @@
 #include <tuple>
 #include "include/complex_number.h"
 
-typedef testing::TestWithParam<std::tuple<double, double, double, double, double, double>> Gusarova_Daria_ComplexNumberTest_Param;
+typedef testing::TestWithParam<std::tuple<double, double, double, double, double, double>> 
+Gusarova_Daria_ComplexNumberTest_Param;
 
 TEST_P(Gusarova_Daria_ComplexNumberTest_Param, Subtract_Zero) {
     // Arrange
@@ -66,7 +67,7 @@ TEST_P(Gusarova_Daria_ComplexNumberTest_Param, Associative_Law_For_Mult) {
 INSTANTIATE_TEST_CASE_P(/**/, Gusarova_Daria_ComplexNumberTest_Param,
     testing::Values(
     std::make_tuple(1.5, 6.8, 7.3, 0.35, 18.0, 14.5),
-    std::make_tuple(-8.0, 4.2, -0.3, 11.0, 2.8, -9.5 ),
+    std::make_tuple(-8.0, 4.2, -0.3, 11.0, 2.8, -9.5),
     std::make_tuple(24.0, -3.4, 2.1, 18.0, -9.7, 0.81),
     std::make_tuple(-20.6, 0.18, 5.4, 3.56, -0.7, 19.0),
     std::make_tuple(4.2, 8.0, -1.2, 45.9, -4.0, 13.6)
